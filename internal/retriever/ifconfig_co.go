83dccbdaf286901d076f7113d664f75648e25833
Felix Stieglitz <6287093+tinyoverflow@users.noreply.github.com>
2026-03-20 09:49:34 +0100

    fix(retriever/ifconfigco): json error check not returning proper error

--- internal/retriever/ifconfig_co.go
+++ internal/retriever/ifconfig_co.go
@@ -5,19 +5,30 @@ import (
 	"net/http"
 )
 
-type IfConfigRetriever struct{}
+type IfConfigRetriever struct {
+	client *http.Client
+	url    string
+}
 
 func (r IfConfigRetriever) GetIPAddress() (string, error) {
-	client := http.DefaultClient
+	client := r.client
+	if client == nil {
+		client = http.DefaultClient
+	}
 
-	res, err := client.Get("https://ifconfig.co/json")
+	url := r.url
+	if url == "" {
+		url = "https://ifconfig.co/json"
+	}
+
+	res, err := client.Get(url)
 	if err != nil {
 		return "", err
 	}
 	defer res.Body.Close()
 
 	var data map[string]any
-	if json.NewDecoder(res.Body).Decode(&data) != nil {
+	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
 		return "", err
 	}
 
